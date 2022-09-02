ebaa616b4da34a808fc1c942e9080619ca3647c7
Sousa <willian.marinho@sealedair.com>
2022-09-02 19:25:56 -0300

    Galeria de imagem fixed

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -3,21 +3,18 @@ import Image from "next/image";
 
 export default function gallery() {
     const images = [
-        "/../public/assets/gallery/img-gallery-1.jpeg",
-        "/../public/assets/gallery/img-gallery-2.jpeg",
-        "/../public/assets/gallery/img-gallery-3.jpeg",
-        "/../public/assets/gallery/img-gallery-4.jpeg",
-        "/../public/assets/gallery/img-gallery-5.jpeg",
-        "/../public/assets/gallery/img-gallery-6.jpeg",
-        "/../public/assets/gallery/img-gallery-7.jpeg",
-        "/../public/assets/gallery/img-gallery-8.jpeg",
-        "/../public/assets/gallery/img-gallery-9.jpeg",
-        "/../public/assets/gallery/img-gallery-10.jpeg",
-        "/../public/assets/gallery/img-gallery-11.jpeg",
-        "/../public/assets/gallery/img-gallery-12.jpeg",
-        "/../public/assets/gallery/img-gallery-13.jpeg",
-        "/../public/assets/gallery/img-gallery-14.jpeg",
-        "/../public/assets/gallery/img-gallery-15.jpeg",
+        "/assets/gallery/img-gallery-1.jpeg",
+        "/assets/gallery/img-gallery-2.jpeg",
+        "/assets/gallery/img-gallery-3.jpeg",
+        "/assets/gallery/img-gallery-5.jpeg",
+        "/assets/gallery/img-gallery-6.jpeg",
+        "/assets/gallery/img-gallery-7.jpeg",
+        "/assets/gallery/img-gallery-8.jpeg",
+        "/assets/gallery/img-gallery-11.jpeg",
+        "/assets/gallery/img-gallery-12.jpeg",
+        "/assets/gallery/img-gallery-13.jpeg",
+        "/assets/gallery/img-gallery-14.jpeg",
+        "/assets/gallery/img-gallery-15.jpeg",
     ]
     return <Servicos images={images} />
 }
