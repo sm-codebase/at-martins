031953533669dde9e814fc28df6fb720758c3cf9
Willian <willian@sousasmidia.com.br>
2022-09-03 16:10:58 -0300

    visible sensor

--- components/Servicos.jsx
+++ components/Servicos.jsx
@@ -1,3 +1,5 @@
+import Zoom from "next-image-zoom";
+import Image from "next/image";
 import { useState } from "react";
 import VisibilitySensor from "react-visibility-sensor";
 
@@ -45,10 +47,10 @@ function GridGalleryCard({ imageUrl, show }) {
       <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
         <div className="absolute inset-0 bg-black opacity-70"></div>
         <div className="mx-auto text-white z-10 self-center uppercase tracking-widest text-sm">
-          Hello World
+          AT Martins
         </div>
       </div>
-      <img src={imageUrl} alt="" />
+      <Zoom src={imageUrl} layout='responsive' width='500' height='400' /> 
     </div>
   );
 }
