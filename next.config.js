f4624b5a68889717775a2d41e7ea5081ee17ccca
Willian <willian@sousasmidia.com.br>
2022-09-12 10:51:06 -0300

    lib fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env: {
+    GOOGLE_ANALYTICS_ID: 'G-92MMYP6VCK'
+  }
 }
+
 
 module.exports = nextConfig
