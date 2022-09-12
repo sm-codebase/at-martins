8ff89b5fce1e12a8ea10b0b909f105318a547e09
Willian <willian@sousasmidia.com.br>
2022-09-12 10:45:43 -0300

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
 import Navbar from '../components/Navbar';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import FloatingWhatsApp from 'react-floating-whatsapp'
 import Footer from '../components/Footer'
+import Script from 'next/script';
+
+import * as ga from '../lib/google-analytics'
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() =>{
+    const handleRouteChange = (url) => {
+      ga.pageView(url)
+    }
+
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () =>
+        router.events.off('routeChangeComplete', handleRouteChange)
+  }, [router.events])
   return (
     <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} strategy='afterInteractive'/>
+      <Script id='google-analytics-script' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        `}
+
+      </Script>
       <Navbar />
       <Component {...pageProps} />
       
