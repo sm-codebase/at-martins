c340a3e4dba985a6cd7d40a4233d61268c4cf122
Willian <willian@sousasmidia.com.br>
2022-09-12 12:40:01 -0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'http://atmartinscaldeiraria.com.br/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
